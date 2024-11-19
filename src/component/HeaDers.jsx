c2eeab488e9ae89275926ff58e067f201b6d01b0
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-11-19 10:39:16 +0100

    version 1.1

--- src/component/HeaDers.jsx
+++ src/component/HeaDers.jsx
@@ -12,8 +12,32 @@ const Header = () => {
              
               <img src={logo} alt="Logo" className="w-[80px] z-[60] " />
             </div>
+           
+
+            <nav className={`flex flex-col gap-10 md:flex-row lg:items-center md:items-center  fixed lg:relative md:relative top-0 bg-[#5B0017] pt-40 md:pt-0 lg:pt-0 pl-5 lg:pl-0 md:pl-0 z-50 h-full md:top-0 left-0 w-[30%]  md:bg-transparent transition-transform ${
+            isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
+          }`}>
+              <Link to="/HeroSection" className="font-CotorisBold text-[21px] hover:underline">
+                About
+              </Link>
+              <Link to="/MenuContainer" className="font-CotorisBold text-[21px] hover:underline">
+                Menu
+              </Link>
+              <Link to="/OurStory"  className="font-CotorisBold text-[21px] hover:underline">
+                Venue
+              </Link>
+              <a href="#events" className="font-CotorisBold text-[21px] hover:underline">
+                Events
+              </a>
+            </nav>
+            <div className="flex ">
+            <button className="bg-[#5B0017] text-white px-4 py-2 rounded-lg hover:bg-[#70011d] ">
+              Book Now
+            </button>
+            <img className="h-10 " src={Pat} alt="" />
+            </div>
             <div
-          className="text-white md:hidden cursor-pointer"
+          className="text-[#5B0017] md:hidden cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
         >
           <svg
@@ -39,29 +63,6 @@ const Header = () => {
             )}
           </svg>
         </div>
-
-            <nav className={`flex flex-col gap-10 md:flex-row lg:items-center md:items-center  fixed lg:relative md:relative top-0 bg-[#5B0017] pt-40 md:pt-0 lg:pt-0 pl-5 lg:pl-0 md:pl-0 z-50 h-full md:top-0 left-0 w-[30%]  md:bg-transparent transition-transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
-          }`}>
-              <Link to="/HeroSection" className="font-CotorisBold text-[21px] hover:underline">
-                About
-              </Link>
-              <Link to="/MenuContainer" className="font-CotorisBold text-[21px] hover:underline">
-                Menu
-              </Link>
-              <Link to="/OurStory"  className="font-CotorisBold text-[21px] hover:underline">
-                Venue
-              </Link>
-              <a href="#events" className="font-CotorisBold text-[21px] hover:underline">
-                Events
-              </a>
-            </nav>
-            <div className="flex ">
-            <button className="bg-[#5B0017] text-white px-4 py-2 rounded-lg hover:bg-[#70011d] ">
-              Book Now
-            </button>
-            <img className="h-10 " src={Pat} alt="" />
-            </div>
           </header>
    );
  };
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -11,8 +11,33 @@ const Header = () => {
              
               <img src={logo} alt="Logo" className="w-[80px] z-[60] " />
             </div>
+           
+
+
+            <nav className={`flex flex-col gap-10 md:flex-row lg:items-center md:items-center  fixed lg:relative md:relative top-0 bg-[#CC9D2F] pt-40 md:pt-0 lg:pt-0 pl-5 lg:pl-0 md:pl-0 z-50 h-full md:top-0 left-0 w-[30%] md:w-auto  md:bg-transparent transition-transform ${
+            isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
+          }`}>
+              <Link to="/HeroSection" className="font-CotorisBold text-[21px] hover:underline">
+                About
+              </Link>
+              <Link to="/MenuContainer" className="font-CotorisBold text-[21px] hover:underline">
+                Menu
+              </Link>
+              <Link to="/OurStory"  className="font-CotorisBold text-[21px] hover:underline">
+                Venue
+              </Link>
+              <a href="#events" className="font-CotorisBold text-[21px] hover:underline">
+                Events
+              </a>
+            </nav>
+            <div className="flex ">
+            <button className="bg-[#C8A42D] text-white px-4 py-2 rounded-lg hover:bg-[#D2AF4A] flex items-center ">
+              Book Now
+            </button>
+            <img className="h-10 " src={Pat} alt="" />
+            </div>
             <div
-          className="text-white md:hidden cursor-pointer"
+          className="text-[#C8A42D] md:hidden cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
         >
           <svg
@@ -38,30 +63,6 @@ const Header = () => {
             )}
           </svg>
         </div>
-
-
-            <nav className={`flex flex-col gap-10 md:flex-row lg:items-center md:items-center  fixed lg:relative md:relative top-0 bg-[#CC9D2F] pt-40 md:pt-0 lg:pt-0 pl-5 lg:pl-0 md:pl-0 z-50 h-full md:top-0 left-0 w-[30%] md:w-auto  md:bg-transparent transition-transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
-          }`}>
-              <Link to="/HeroSection" className="font-CotorisBold text-[21px] hover:underline">
-                About
-              </Link>
-              <Link to="/MenuContainer" className="font-CotorisBold text-[21px] hover:underline">
-                Menu
-              </Link>
-              <Link to="/OurStory"  className="font-CotorisBold text-[21px] hover:underline">
-                Venue
-              </Link>
-              <a href="#events" className="font-CotorisBold text-[21px] hover:underline">
-                Events
-              </a>
-            </nav>
-            <div className="flex ">
-            <button className="bg-[#C8A42D] text-white px-4 py-2 rounded-lg hover:bg-[#D2AF4A] flex items-center ">
-              Book Now
-            </button>
-            <img className="h-10 " src={Pat} alt="" />
-            </div>
           </header>
    );
  };
