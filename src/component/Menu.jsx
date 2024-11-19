e4a980d4792ffc1dbe7c7dc330ad10787985b4bd
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-11-19 01:36:50 +0100

    version 1.1

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -27,13 +27,11 @@ const MenuSection = () => {
         <h2 className="font-CotorisBold text-3xl lg:text-6xl md:text-5xl mb-8 max-w-2xl mx-auto leading-tight">
           A unique menu that reflects the true essence of the Saudi cuisine
         </h2>
-        <div className='flex justify-center '>
-        <div className='relative justify-center'> 
-       <div className='bg-[#5B0017] border-2 border-[#CC9D2F] lg:w-[131px] h-[50px] relative top-3 -left-2  '>
+           <div className=' flex justify-center w-full items-end '> 
+       <div className='bg-[#5B0017] border-2 border-[#CC9D2F] lg:w-[131px] w-full  h-[50px] relative top-3 -left-2  '>
         <button className="font-CotorisBold mt-6 px-6 py-3 w-full   bg-[#CC9D2F] border border-[#5B0017] text-[#5B0017] font-medium  hover:bg-[#5B0017] hover:text-white transition duration-300 relative -top-[29px] left-[2px] ">
           Our Menu 
         </button>
-        </div>
         </div>
         </div>
         
